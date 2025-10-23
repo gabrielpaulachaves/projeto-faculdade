c67b555638a4384fa1daa624a82210098cb99ec2
Gabriel <bielpchaves@gmail.com>
2025-10-23 00:07:14 -0300

    t

---
+++ pagina 4/script.js
@@ -0,0 +1,44 @@
+class MobileNavBar {
+    constructor(menuicon, navitens, navlinks) {
+        this.menuicon = document.querySelector(menuicon);
+        this.navitens = document.querySelector(navitens);
+        this.navlinks = document.querySelectorAll(navlinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animatelinks() {
+        this.navlinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navlinksFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+
+    handleClick() {
+        this.navitens.classList.toggle(this.activeClass);
+        this.menuicon.classList.toggle(this.activeClass);
+        this.animatelinks();
+        document.body.classList.toggle("no-scroll"); // bloqueia scroll
+    }
+
+    addClickEvent() {
+        this.menuicon.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.menuicon) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavBar(
+    "#menu-icon",
+    "#nav-itens",
+    "#nav-itens li"
+);
+
+mobileNavbar.init();
